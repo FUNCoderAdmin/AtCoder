8cd9aae759bba6f44f97e26376701e45ed7188c4
FUN-Morikubo <b1022210@fun.ac.jp>
2025-05-15 19:57:08 +0900

    add abc326/solution2.py

---
+++ abc/abc326/c/solution_2.py
@@ -0,0 +1,13 @@
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+A.sort()
+
+ans = 0
+r = 0
+# A[l] に左端を投げると [l, r) 番目のプレゼントを捕まえられる
+for l in range(N):
+    while r < N and A[r] < A[l] + M:
+        r += 1
+    ans = max(ans, r - l)
+
+print(ans)
